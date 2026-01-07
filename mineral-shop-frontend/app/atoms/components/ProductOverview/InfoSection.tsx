562684aea69666bc6c126a621a22f96049120a7e
Bartosz <golebiowski.bartosz.21@gmail.com>
2026-01-07 17:21:43 +0100

    styles of product-info section

--- mineral-shop-frontend/app/atoms/components/ProductOverview/InfoSection.tsx
+++ mineral-shop-frontend/app/atoms/components/ProductOverview/InfoSection.tsx
@@ -7,11 +7,18 @@ interface InfoSectionProps {
 const InfoSection: React.FC<InfoSectionProps> = ({ product }) => {
   return (
     <>
-      <h1>{product.name}</h1>
-      <p>{product.price} zł</p>
-      <p>{product.stones.map(s => s.name)}</p>
-      <p>Rozmiar: {product.size.name}</p>
-      <p>{product.description}</p>
+      <p className="product-title mb-3">{product.name}</p>
+      <p className="mb-3">{product.price} zł</p>
+      <p className="size mb-4">Rozmiar: {product.size.name}</p>
+      <div className="pill-container mb-4">
+        {product.stones.map((stone, index) => 
+          <div key={index} className="pill">
+            {stone.name}
+          </div>
+        )}
+      </div>
+
+      <p className="description mb-4">{product.description}</p>
     </>
   )
 };
